e62e3adf24477411ae933f8641a4eb55e6038c67
zios <khyunwook0707@gmail.com>
2024-03-06 08:26:50 +0900

    chore: (#3) null 허용 제거

--- repo-persistence/src/main/kotlin/kotlinRepo/reporepo/persistence/user/repository/UserJpaRepository.kt
+++ repo-persistence/src/main/kotlin/kotlinRepo/reporepo/persistence/user/repository/UserJpaRepository.kt
@@ -1,8 +1,7 @@
 package kotlinRepo.reporepo.persistence.user.repository
 
 import kotlinRepo.reporepo.persistence.user.entity.UserJpaEntity
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository    
 import org.springframework.stereotype.Repository
 import java.util.*
 
@@ -13,7 +12,7 @@ interface UserJpaRepository : JpaRepository<UserJpaEntity, UUID> {
 
     fun findByUsername(username: String?) : UserJpaEntity
 
-    fun findByEmail(email: String?) : UserJpaEntity
+    fun findByEmail(email: String) : UserJpaEntity
 
     fun findUserJpaEntityById(userId: UUID) : UserJpaEntity
 
