13979b533078c8d76d3e4dad84e0b37cd4fa03e9
zios <khyunwook0707@gmail.com>
2024-03-08 15:18:41 +0900

    refactor: (#4) findAllOrderByCreateAtDesc 쿼리dsl 사용

--- repo-persistence/src/main/kotlin/kotlinRepo/reporepo/persistence/notice/NoticePersistenceAdapter.kt
+++ repo-persistence/src/main/kotlin/kotlinRepo/reporepo/persistence/notice/NoticePersistenceAdapter.kt
@@ -1,23 +1,32 @@
 package kotlinRepo.reporepo.persistence.notice
 
+import com.querydsl.jpa.impl.JPAQueryFactory
 import kotlinRepo.reporepo.domain.notice.model.Notice
 import kotlinRepo.reporepo.domain.notice.spi.NoticePort
+import kotlinRepo.reporepo.persistence.notice.entity.QNoticeJpaEntity
 import kotlinRepo.reporepo.persistence.notice.mapper.NoticeMapper
 import kotlinRepo.reporepo.persistence.notice.repository.NoticeJpaRepository
 import org.springframework.stereotype.Component
-import kotlin.streams.toList
+import java.util.UUID
 
 @Component
 class NoticePersistenceAdapter(
     private val noticeMapper: NoticeMapper,
-    private val noticeJpaRepository: NoticeJpaRepository
+    private val noticeJpaRepository: NoticeJpaRepository,
+    private val jpaQueryFactory: JPAQueryFactory
 ) : NoticePort {
 
-    override fun findAllOrderByCreateAtDesc() =
-        noticeJpaRepository.findAll().map {noticeMapper.toDomain(it)!!}
-            .stream().sorted {n1, n2 -> n1.createAt.compareTo(n2.createAt)}
-            .toList().filterNotNull()
+    override fun findAllOrderByCreateAtDesc() : List<Notice> {
+        val notice = QNoticeJpaEntity.noticeJpaEntity
 
+        return jpaQueryFactory
+            .selectFrom(notice)
+            .orderBy(notice.createdAt.desc())
+            .fetch()
+            .map {
+                noticeMapper.toDomain(it)!!
+            }
+    }
 
     override fun saveNotice(notice: Notice): Notice = noticeMapper.toDomain(
         noticeJpaRepository.save(
