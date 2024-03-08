92c4ca2ee7a71809cde291a81f02044fc9bcbb34
zios <khyunwook0707@gmail.com>
2024-03-08 15:55:46 +0900

    feat: (#6) QueryNoticePort - findById

--- repo-core/src/main/kotlin/kotlinRepo/reporepo/domain/notice/spi/QueryNoticePort.kt
+++ repo-core/src/main/kotlin/kotlinRepo/reporepo/domain/notice/spi/QueryNoticePort.kt
@@ -1,8 +1,12 @@
 package kotlinRepo.reporepo.domain.notice.spi
 
 import kotlinRepo.reporepo.domain.notice.model.Notice
+import java.util.UUID
 
 interface QueryNoticePort {
 
     fun findAllOrderByCreateAtDesc() : List<Notice>
+
+    fun findById(noticeId: UUID): Notice?
+
 }
