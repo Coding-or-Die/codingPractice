192e993e14b62e3b964d572032113711089800bd
zios <khyunwook0707@gmail.com>
2024-03-08 15:53:46 +0900

    feat: (#6) NoticeWebAdapter

--- repo-presentation/src/main/kotlin/kotlinRepo/reporepo/domain/notice/NoticeWebAdapter.kt
+++ repo-presentation/src/main/kotlin/kotlinRepo/reporepo/domain/notice/NoticeWebAdapter.kt
@@ -1,18 +1,27 @@
 package kotlinRepo.reporepo.domain.notice
 
 import kotlinRepo.reporepo.domain.notice.dto.response.NoticeResponse
+import kotlinRepo.reporepo.domain.notice.usecase.GetNoticeDetailsUseCase
 import kotlinRepo.reporepo.domain.notice.usecase.GetNoticeListUseCase
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 @RequestMapping("/notice")
 class NoticeWebAdapter (
-    private val getNoticeListUseCase: GetNoticeListUseCase
+    private val getNoticeListUseCase: GetNoticeListUseCase,
+    private val getNoticeDetailsUseCase: GetNoticeDetailsUseCase,
 ) {
     @GetMapping
     fun getNoticeList(): List<NoticeResponse> {
         return getNoticeListUseCase.execute()
+    }
+
+    @GetMapping("/{notice-id}")
+    fun getNoticeDetails(@PathVariable("notice-id") noticeId: UUID) : NoticeResponse {
+        return getNoticeDetailsUseCase.execute(noticeId)
     }
 }
