f93305c47506d7853c7b67d0a761e748d7324aaa
zios <khyunwook0707@gmail.com>
2024-03-08 15:19:32 +0900

    setting: (#4) 쿼리dsl 설정

---
+++ repo-persistence/src/main/kotlin/kotlinRepo/reporepo/global/config/QuerydslConfig.kt
@@ -0,0 +1,15 @@
+package kotlinRepo.reporepo.global.config
+
+import com.querydsl.jpa.JPQLTemplates
+import com.querydsl.jpa.impl.JPAQueryFactory
+import jakarta.persistence.EntityManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class QuerydslConfig (
+    private val entityManager: EntityManager
+) {
+    @Bean
+    protected fun queryFactory() = JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager)
+}
