8d000261a5e277851e6f397d4e9bb1550a16fdad
zios <khyunwook0707@gmail.com>
2024-03-11 16:47:42 +0900

    chore: (#4) 엔드포인트 복수명사로 변경

--- repo-presentation/src/main/kotlin/kotlinRepo/reporepo/domain/user/UserWebAdapter.kt
+++ repo-presentation/src/main/kotlin/kotlinRepo/reporepo/domain/user/UserWebAdapter.kt
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/user")
+@RequestMapping("/users")
 @RestController
 class UserWebAdapter (
     private val signupUseCase: SignupUseCase,
